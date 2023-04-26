ce5f33d16c6920a9f2550ba1760e8c91afe0b959
juliaRocha18 <131920028+juliaRocha18@users.noreply.github.com>
2023-04-26 18:34:08 -0300

    Herança slide 5

---
+++ Main.java
@@ -0,0 +1,42 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+	Pessoa p1 = new Pessoa();
+		
+	p1.setNome("Ana");
+	p1.setIdade(25);
+	p1.setPeso(47);
+	
+	Pessoa p2 = new Pessoa();
+	
+	p2.setNome ("Carlos");
+	p2.setIdade(35);
+	p2.setPeso (87);
+	
+	Pessoa p3 = new Pessoa();
+	
+	p3.setNome ("lilian");
+	p3.setIdade(27);
+	p3.setPeso (67);
+	
+	 System.out.println ("nome"+p1.getNome());
+	 System.out.println ("nome"+p1.getIdade());
+	 System.out.println ("nome"+p1.getPeso());
+	 
+	 System.out.println ("nome"+p2.getNome());
+	 System.out.println ("nome"+p2.getIdade());
+	 System.out.println ("nome"+p2.getPeso());
+	 
+	 System.out.println ("nome"+p3.getNome());
+	 System.out.println ("nome"+p3.getIdade());
+	 System.out.println ("nome"+p3.getPeso());
+
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
